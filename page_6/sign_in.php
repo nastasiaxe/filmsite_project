e014c5b96e4dafdcf5659c61aeab4886b28d100f
nastasiaxe <131003197+nastasiaxe@users.noreply.github.com>
2023-05-25 15:05:59 +0300

    Add files via upload

---
+++ page_6/sign_in.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require_once('connect.php');
+
+
+$email = $_POST['email_us'];
+$password = $_POST['password_us'];
+
+
+if(empty($email) || empty($password)){
+
+    $_SESSION['message'] = 'Заполните все поля';
+    header('Location: page_6.php'); 
+
+} else {
+
+    $check_user = mysqli_query($connection, "SELECT * FROM `users` WHERE `email` = '$email' AND `password` = '$password'");
+    $data = mysqli_fetch_assoc($check_user);
+
+    if($data['password'] === $_POST['password_us']) {
+
+        header("Location: ../page_1/page_1.html"); exit(); // на время, потом будет отправлять на главную страницу
+
+    } else {
+
+        $_SESSION['message'] = 'Неверный логин или пароль';
+        header('Location: page_6.php');
+
+    }
+}
+?>
---
+++ page_6/sign_up.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+require_once('../connect.php');
+
+$name = $_POST['name'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
+$email = $_POST['email'];
+
+if ($password != $confirm_password) {
+    $_SESSION['message'] = 'Пароли не совпадают';
+    header('Location: page_6.php');
+} else {
+    $path = 'uploads/' . time() . $_FILES['pic']['name'];
+    if (!move_uploaded_file($_FILES['pic']['tmp_name'], $path)) {
+        $_SESSION['message'] = 'Ошибка при загрузке изображения';
+        header('Location: page_6.php');
+    }
+   
+    mysqli_query($connection, "INSERT INTO `users` (`user_id`, `name`, `password`, `email`, `user_pic`) 
+    VALUES (NULL, '$name', '$password', '$email', '$path')");
+
+    $_SESSION['message'] = 'Регистрация прошла успешно';
+    header('Location: page_6.php');
+
+}
+
+?>
